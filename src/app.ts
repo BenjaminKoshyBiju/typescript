b3d360fe6f218436d6d5616a68c470f44a119450
BenjaminKoshyBiju <bennn455@gmail.com>
2023-11-10 18:10:47 +0530

    chaining requests

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express ,{Request,Response} from 'express'
 
 // import routes from '../router/routes'
 
@@ -8,7 +8,7 @@ const port=3000
 // app.use('/',routes)
 
 app.use(express.json())
-app.get('/',(req,res)=>{
+app.get('/',(req:Request,res:Response)=>{
     res.send('Helllllllooooos')
 });
 
@@ -16,6 +16,26 @@ app.post('/api/data',(req,res)=>{
     console.log(req.body);
     return res.sendStatus(200)
 });
+// we can chain requests like this it can be used for CRUD operations
+app.route('/api/books')                 
+.get((req:Request,res:Response)=>{              //get a book
+    return res.send('This is a get request')
+})
+.post((req:Request,res:Response)=>{             //insert a book
+    return res.send('This is a post request')
+})
+.put((req:Request,res:Response)=>{              //update a book
+    return res.send('This is a put request')
+})
+
+.delete((req:Request,res:Response)=>{              //delete a book
+    return res.send('This is a delete request')
+})
+.all((req:Request,res:Response)=>{             
+    res.sendStatus(404);
+    
+    
+})
 
 
 app.listen(port,()=>{
